694cad0d98907af79ff1523baec30efb7ed219fb
Alan Verbner <alverbner@gmail.com>
2021-06-02 15:04:24 -0300

    fix: make nix build run
    Closes #1

--- nix/packages.nix
+++ nix/packages.nix
@@ -6,7 +6,6 @@ let
     sources = import ./sources.nix;
     nodejs = pkgs.nodejs-12_x;
     nix-inclusive = pkgs.callPackage "${self.sources.nix-inclusive}/inclusive.nix" {};
-    inherit (self.yarn-static.passthru) offlinecache;
     multisig-coordination-server = self.callPackage ./multisig-coordination-server.nix {};
   };
 in pkgs.lib.makeScope pkgs.newScope packages
